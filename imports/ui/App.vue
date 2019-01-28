17e8532ec6746c0146807657b387ae0dc707ffe2
Rija <rakinakuy@hotmail.com>
2019-01-28 19:11:06 +0000

    app on

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,17 +1,12 @@
 <script>
     export default{
         name: 'App',
-        computed: {
-            color(){
-                return this.$store.state.themeColor
-            }
-        }
     }
 </script>
 <template>
     <v-app dark>
         <v-content>
-            <v-btn small>{{color}}</v-btn>
+            <v-btn small>Go</v-btn>
         </v-content>
     </v-app>
 </template>
