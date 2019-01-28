080182089050aaa560d9d3d672c80da41401af71
Rija <rakinakuy@hotmail.com>
2019-01-28 18:48:58 +0000

    add vuex themeColor

--- client/index.js
+++ client/index.js
@@ -7,9 +7,12 @@ import 'vuetify/dist/vuetify.min.css'
 
 import App from '../imports/ui/App.vue'
 
+import store from '../imports/api/store'
+
 Meteor.startup(()=>{
     new Vue({
         el:'#app',
-        render: h=>h(App)
+        render: h=>h(App),
+        store
     })
 })
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,8 +1,17 @@
-<script></script>
+<script>
+    export default{
+        name: 'App',
+        computed: {
+            color(){
+                return $store.state.themeColor
+            }
+        }
+    }
+</script>
 <template>
     <v-app>
         <v-content>
-            <v-btn small>Win</v-btn>
+            <v-btn small>{{color}}</v-btn>
         </v-content>
     </v-app>
 </template>
