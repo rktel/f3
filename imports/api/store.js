f6cb2fac99895394f31cf1a918f9c18b25187557
Rija <rakinakuy@hotmail.com>
2019-01-28 22:00:50 +0000

    log 3

--- imports/api/store.js
+++ imports/api/store.js
@@ -3,8 +3,6 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-console.log(Meteor.settings.public.appColors)
-
 export default new Vuex.Store({
     state: {
         appColors: Meteor.settings.public.appColors
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,15 +1,17 @@
 <script>
     export default{
         name: 'App',
-        mounted(){
-            console.log(this.$store.state.appColors)
+        computed: {
+            colors() => this.$store.state.appColors
         }
     }
 </script>
 <template>
     <v-app class="indigo darken-4">
         <v-content>
-            <v-btn small>Go</v-btn>
+            <ul>
+                <li v-for="color in colors">{{color}}</li>
+            </ul>
         </v-content>
     </v-app>
 </template>
