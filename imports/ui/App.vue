3a1f4fb17e42c52669b4ea140afce06a340cee95
Rija <rakinakuy@hotmail.com>
2019-01-29 16:20:07 +0000

    add vue-router

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,37 +1,13 @@
 <script>
     export default{
         name: 'App',
-        mounted(){
-            console.log(this.$store.state)
-        },
-        data(){
-            return{
-                appColor: 'red',
-                appColorSelected:''
-            }
-        },
-        computed:{
-            colors(){
-                return this.$store.state.appColors
-            }
-        },
-        methods:{
-            changeAppColor(){
-                console.log('On changeAppColor', this.appColorSelected)
-                this.appColor = this.appColorSelected
-            }
-        }
+
     }
 </script>
 <template>
-    <v-app class="text--red" :class="appColor">
+    <v-app>
         <v-content>
-                <v-combobox
-                v-model="appColorSelected"
-                :items="colors"
-                label="App colors"
-                ></v-combobox>
-                <v-btn @click="changeAppColor">Change Color</v-btn>
+
         </v-content>
     </v-app>
 </template>
