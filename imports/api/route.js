a4b4ea65b3bb567bb113140afc81a9963d1c4858
Rija <rakinakuy@hotmail.com>
2019-01-29 22:18:17 +0000

    router 4

--- imports/api/route.js
+++ imports/api/route.js
@@ -7,8 +7,9 @@ import Login from '../ui/views/Login.vue'
 import F3 from '../ui/views/F3.vue'
 
 const routes = [
+    { path: '/', redirect: '/f3' },
     { path: '/login', component: Login },
-    { path: '/f3', component: F3},
+    { path: '/f3', component: F3 },
 ]
 
 export default new VueRouter({
