84ae29f3d3aa1a9e9cb5d1139474925bafd8b6f1
Rija <rakinakuy@hotmail.com>
2019-01-29 22:23:48 +0000

    router 6

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -6,7 +6,7 @@
 </script>
 <template>
     <v-app>
-    <v-toolbar></v-toolbar>
+    <v-toolbar dense></v-toolbar>
         <v-content>
             <router-view></router-view>
         </v-content>
