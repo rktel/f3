fa3cd781f2a65804df5c5f429da3dbb0a2ab323c
Rija <rakinakuy@hotmail.com>
2019-01-30 19:46:43 +0000

    container 13

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -8,19 +8,25 @@
         <v-toolbar dense color="amber" absolute clipped-left app></v-toolbar>
         <v-content>
             <section class="contenedor">
-                <div class="item">1</div>
-                <div class="item">2</div>
+                <div class="itemOne">1</div>
+                <div class="itemTwo">2</div>
             </section>
         </v-content>
     </v-app>
 </template>
 <style scoped>
     .contenedor{
-        display: flex
+        display: flex;
+        flex-direction: row;
     }
-    .item{
+    .itemOne{
         background-color: teal;
-        height: 200px;
-        width: 200px;
+        flex: 3 1 auto;
+        align-self: stretch;
+    }
+    .itemTwo{
+        background-color: indigo;
+        flex: 7 1 auto;
+        align-self: stretch;
     }
 </style>
