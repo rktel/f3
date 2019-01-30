c99e3f00de4c19caec8caf3e15a7a237ce87b5e2
Rija <rakinakuy@hotmail.com>
2019-01-30 16:37:04 +0000

    toolbar and drawer 5

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -6,11 +6,13 @@
 </script>
 <template>
     <v-app>
-    <v-navigation-drawer v-model="drawer" fixed clipped class="grey lighten-4 elevation-4" width="480" app>
-        <h1>Navbar</h1>
-    </v-navigation-drawer>
-    <v-toolbar dense color="amber" absolute clipped-left app></v-toolbar>
+
+        <v-toolbar dense color="amber" absolute clipped-left app></v-toolbar>
+
         <v-content>
+            <v-navigation-drawer v-model="drawer" fixed clipped class="grey lighten-4 elevation-4" width="480" app>
+                <h1>Navbar</h1>
+            </v-navigation-drawer>
             <router-view></router-view>
         </v-content>
     </v-app>
