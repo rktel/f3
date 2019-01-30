026a2e9d85c6734ceebb023a71c5a1cf0b8df16b
Rija <rakinakuy@hotmail.com>
2019-01-30 19:31:22 +0000

    container 10

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,33 +1,22 @@
 <script>
     export default{
         name: 'App',
-        computed:{
-            binding(){
-                const binding = {}
-                if(this.$vuetify.breakpoint.smAndDown) binding.column = true
-                return binding
-            }
-        }
     }
 </script>
 <template>
     <v-app>
         <v-toolbar dense color="amber" absolute clipped-left app></v-toolbar>
-        <v-content>
-
-                <v-layout v-bind="binding">
-                    <v-flex flex-grow>
-                        <v-card dark tile flat color="error">
-                        <v-card-text>#1</v-card-text>
-                        </v-card>
-                    </v-flex>
-                    <v-flex flex-grow>
-                        <v-card dark tile flat color="red darken-4">
-                        <v-card-text>#2</v-card-text>
-                        </v-card>
-                    </v-flex>
-                </v-layout>
-
-        </v-content>
+        <section class="contenedor">
+            <div>1</div>
+            <div>2</div>
+        </section>
     </v-app>
 </template>
+<style scoped>
+    .contenedor{
+        display: flex
+    }
+    .contenedor > div{
+        background-color: teal
+    }
+</style>
