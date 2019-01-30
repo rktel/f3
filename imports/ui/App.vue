09e32512de251a54ac3b0933feba9d1a26b2e154
Rija <rakinakuy@hotmail.com>
2019-01-30 20:33:25 +0000

    container 23

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -14,7 +14,21 @@
             right
             app
             >
+            <v-toolbar flat class="transparent">
+                <v-list class="pa-0">
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
+                    <img src="https://randomuser.me/api/portraits/men/85.jpg">
+                    </v-list-tile-avatar>
+
+                    <v-list-tile-content>
+                    <v-list-tile-title>John Leider</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                </v-list>
+            </v-toolbar>
             <v-list dense>
+                <v-divider></v-divider>
                 <v-list-tile @click="">
                 <v-list-tile-action>
                     <v-icon>home</v-icon>
@@ -35,7 +49,7 @@
         </v-navigation-drawer>
         <v-toolbar color="cyan" dark fixed dense app>
             <v-spacer></v-spacer>
-            <v-toolbar-title>Application</v-toolbar-title>
+            <v-toolbar-title>Figo3</v-toolbar-title>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
         </v-toolbar>
         <v-content>
