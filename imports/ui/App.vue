93c94e9624d3c2dfb0949095f981a5480c8fd864
Rija <rakinakuy@hotmail.com>
2019-01-30 16:52:24 +0000

    toolbar and drawer 8

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -6,12 +6,22 @@
 </script>
 <template>
     <v-app>
-
         <v-toolbar dense color="amber" absolute clipped-left app></v-toolbar>
-
         <v-content>
-            <h1>Hellooooo......</h1>
-            <router-view></router-view>
+            <v-container fluid>
+                <v-layout row>
+                    <v-flex xs6 order-lg2>
+                        <v-card dark tile flat color="error">
+                        <v-card-text>#1</v-card-text>
+                        </v-card>
+                    </v-flex>
+                    <v-flex xs6>
+                        <v-card dark tile flat color="red darken-4">
+                        <v-card-text>#2</v-card-text>
+                        </v-card>
+                    </v-flex>
+                    </v-layout>
+            </v-container>
         </v-content>
     </v-app>
 </template>
