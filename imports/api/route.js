4596f2ff7a956074ce0b980e16582fccea56c101
rktel <rakinakuy@hotmail.com>
2019-02-20 17:37:46 -0500

    add Login a F3

--- imports/api/route.js
+++ imports/api/route.js
@@ -8,8 +8,8 @@ import F3 from '../ui/views/F3.vue'
 
 const routes = [
     { path: '/', redirect: '/f3' },
-    { path: '/login', component: Login },
-    { path: '/f3', component: F3 },
+    { path: '/login', component: Login, name: 'Login' },
+    { path: '/f3', component: F3, name: 'F3' },
 ]
 
 export default new VueRouter({
--- imports/ui/views/Login.vue
+++ imports/ui/views/Login.vue
@@ -1,8 +1,67 @@
-<script>
-    export default{
-        
-    }
-</script>
 <template>
-    <h1>Login</h1>
+  <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card hover light class="pa-4">
+              <v-card-text>
+                <section>
+                  <img src="/img/seclog.png" height="45px">
+                </section>
+                <h1 class="font-weight-bold text-xs-center mb-4">Login</h1>
+                <v-text-field
+                  append-icon="person"
+                  name="login"
+                  label="Usuario"
+                  placeholder="Introduce tu usuario"
+                  type="text"
+                  v-model="username"
+                ></v-text-field>
+                <v-text-field
+                  append-icon="lock"
+                  name="password"
+                  label="Password"
+                  placeholder="Introduce tu password"
+                  type="password"
+                  v-model="password"
+                  @keyup.enter="onLogin"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions class="text-xs-center">
+                <v-btn block @click="onLogin" color="info">entrar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    onLogin() {
+      const { username, password } = this;
+      if (username.length > 0 && password.length > 0) {
+        Meteor.loginWithPassword(username, password, error => {
+          if (!error) {
+            this.$router.push({ name: "Home" });
+          }
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
