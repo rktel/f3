50bad86513db5965e955e0f24b4b1acb06f79ea7
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-14 16:21:59 -0500

    add Last 4

--- server/gps/gpsMethods.js
+++ server/gps/gpsMethods.js
@@ -3,7 +3,7 @@ import { Events, Infos, Last } from '../../imports/api/collections'
 Meteor.methods({
     insertEvent: function (data) {
         Events.insert(data)
-        Last.upsert({ 'data.event.deviceId': data.event.deviceId }, { $set: { data } })
+        Last.upsert({ 'event.device': data.event.deviceID }, { $set: { data } })
     },
     insertInfo: function (data) {
         Infos.insert(data)
