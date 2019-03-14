d00561fc0831767db305add3c274a208d3455e11
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-14 15:48:34 -0500

    upsert Last

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -7,4 +7,4 @@ export const Personal = new Mongo.Collection('personal')
 /**GPS Data */
 export const Events = new Mongo.Collection('events')
 export const Infos = new Mongo.Collection('infos')
-
+export const Last = new Mongo.Collection('last')
--- server/gps/gpsMethods.js
+++ server/gps/gpsMethods.js
@@ -1,10 +1,11 @@
-import { Events, Infos } from '../../imports/api/collections'
+import { Events, Infos, Last } from '../../imports/api/collections'
 
-Meteor.methods({ 
-    insertEvent: function(data) { 
-         Events.insert(data)
+Meteor.methods({
+    insertEvent: function (data) {
+        Events.insert(data)
+        Last.upsert(data.deviceId, data)
     },
-    insertInfo:function(data){
+    insertInfo: function (data) {
         Infos.insert(data)
     }
 });
