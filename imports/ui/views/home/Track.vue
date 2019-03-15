9a800334816b59bb0a61aab9ec865a4735e81f19
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-15 11:05:23 -0500

    add stSyrus GET_DEVICES_ON 4

--- imports/ui/views/home/Track.vue
+++ imports/ui/views/home/Track.vue
@@ -5,23 +5,43 @@ export default {
   name: "Track",
   mounted() {
     stSyrus.on("DEVICES_ON", devicesOn => {
-      this.DEVICES_ON = devicesOn
+      this.DEVICES_ON = devicesOn;
       console.log(devicesOn);
     });
-    stSyrus.emit("GET_DEVICES_ON")
+    stSyrus.emit("GET_DEVICES_ON");
   },
   data: () => ({
-    DEVICES_ON: []
-  })
+    DEVICES_ON: [],
+    deviceFilter: null
+  }),
+  computed: {
+    filteredDevice() {
+      if (!this.deviceFilter) return this.DEVICES_ON;
+      let searchText = this.deviceFilter.toLowerCase();
+      return this.DEVICES_ON.filter(d => {
+        return (
+          d.toLowerCase().includes(searchText) 
+        );
+      });
+    }
+  }
 };
 </script>
 
 <template>
   <section class="contenedor bg-color">
     <div class="itemOne">
-      <h1>Track</h1>
+      <v-toolbar flat class="pt-0 transparent" dark>
+        <v-text-field
+          label="Buscar dispositivo"
+          prepend-icon="search"
+          single-line
+          v-model="deviceFilter"
+          clearable
+        ></v-text-field>
+      </v-toolbar>
       <v-list>
-        <v-list-tile v-for="device in DEVICES_ON" :key="device">{{device}}</v-list-tile>
+        <v-list-tile v-for="device in filteredDevice" :key="device">{{device}}</v-list-tile>
       </v-list>
     </div>
     <div class="itemTwo">2</div>
