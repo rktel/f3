33ec87639f153d8f9e71169c109801769e5f784f
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-15 10:37:49 -0500

    change bg color 10

--- imports/ui/views/Login.vue
+++ imports/ui/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-content class="bg-triangle">
+    <v-content class="bg-color">
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
