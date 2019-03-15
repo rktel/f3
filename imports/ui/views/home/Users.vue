17a1b5e1f6e381aaa27b62365d4509e6dbb75c71
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-15 11:22:28 -0500

    add stSyrus GET_DEVICES_ON 8

--- imports/ui/views/home/Users.vue
+++ imports/ui/views/home/Users.vue
@@ -93,6 +93,7 @@ export default {
     </section>
 
     <section class="itemTwo">
+      <v-divider></v-divider>
       <v-toolbar flat class="pt-0 transparent" dark>
         <v-spacer></v-spacer>
         <v-btn color="green" flat class="white--text" @click="toggleFlagUFD">Agregar
