931ef96cdc998fb80bafcf53753dcf2949851f90
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-18 11:45:50 -0500

    subscribe Devices+ 19

--- imports/ui/views/home/Track.vue
+++ imports/ui/views/home/Track.vue
@@ -102,7 +102,7 @@ export default {
 
             <v-list-tile-content>
               <v-list-tile-title>{{ device.deviceID }}</v-list-tile-title>
-              <v-list-tile-sub-title>{{ addHours(device.connectionTime, -5) }}</v-list-tile-sub-title>
+              <v-list-tile-sub-title>{{ device.connectionTime?addHours(device.connectionTime, -5):'' }}</v-list-tile-sub-title>
             </v-list-tile-content>
 
             <v-list-tile-action>
