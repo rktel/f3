679254dc66c719f7be6a4e808d630c5d83f0ced4
rktel <rakinakuy@hotmail.com>
2019-03-19 16:26:09 -0500

    deviceMessenger 69

--- imports/ui/views/components/DeviceMessenger.vue
+++ imports/ui/views/components/DeviceMessenger.vue
@@ -13,24 +13,39 @@
         </v-toolbar>
 
         <section class="messengerContent" :style="{height: heightDeviceMessenger+'px' }">
-          <div>
-            <beautiful-chat
-              :participants="participants"
-              :titleImageUrl="titleImageUrl"
-              :onMessageWasSent="onMessageWasSent"
-              :messageList="messageList"
-              :newMessagesCount="newMessagesCount"
-              :isOpen="isChatOpen"
-              :close="closeChat"
-              :open="openChat"
-              :showEmoji="true"
-              :showFile="true"
-              :showTypingIndicator="showTypingIndicator"
-              :colors="colors"
-              :alwaysScrollToBottom="alwaysScrollToBottom"
-              :messageStyling="messageStyling"
-            />
+
+          <div
+            class="messengerMsg vuebar-element"
+            v-bar
+            :style="{height: heightDeviceMessenger-64 +'px' }"
+          >
+            <v-timeline>
+              <v-timeline-item v-for="(command,index) in deviceCommands" :key="index">
+                <template v-slot:opposite>
+                  <span>{{restFiveHours(command.sendTime)}}</span>
+                </template>
+                <v-card class="elevation-2">
+                  <v-card-title class="headline">{{ command.command }}</v-card-title>
+                  <v-card-text>{{command.response}}</v-card-text>
+                </v-card>
+              </v-timeline-item>
+            </v-timeline>
           </div>
+
+          <div class="messengerCommand">
+            <v-text-field
+              class="mx-2"
+              v-model="deviceMsg"
+              :append-outer-icon="'send'"
+              clearable
+              label="Message"
+              type="text"
+              @keyup="toUppercase"
+              @keyup.enter.native="sendMsg"
+              @click:append-outer="sendMsg"
+            ></v-text-field>
+          </div>
+          
         </section>
       </v-card>
     </v-dialog>
@@ -67,81 +82,9 @@ export default {
     }
   },
   data: () => ({
-    deviceMsg: null,
-    participants: [
-      {
-        id: "user1",
-        name: "Matteo",
-        imageUrl: "https://avatars3.githubusercontent.com/u/1915989?s=230&v=4"
-      },
-      {
-        id: "user2",
-        name: "Support",
-        imageUrl: "https://avatars3.githubusercontent.com/u/37018832?s=200&v=4"
-      }
-    ], // the list of all the participant of the conversation. `name` is the user name, `id` is used to establish the author of a message, `imageUrl` is supposed to be the user avatar.
-    titleImageUrl:
-      "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png",
-    messageList: [
-      { type: "text", author: `me`, data: { text: `Say yes!` } },
-      { type: "text", author: `user1`, data: { text: `No.` } }
-    ], // the list of the messages to show, can be paginated and adjusted dynamically
-    newMessagesCount: 0,
-    isChatOpen: false, // to determine whether the chat window should be open or closed
-    showTypingIndicator: "", // when set to a value matching the participant.id it shows the typing indicator for the specific user
-    colors: {
-      header: {
-        bg: "#4e8cff",
-        text: "#ffffff"
-      },
-      launcher: {
-        bg: "#4e8cff"
-      },
-      messageList: {
-        bg: "#ffffff"
-      },
-      sentMessage: {
-        bg: "#4e8cff",
-        text: "#ffffff"
-      },
-      receivedMessage: {
-        bg: "#eaeaea",
-        text: "#222222"
-      },
-      userInput: {
-        bg: "#f4f7f9",
-        text: "#565867"
-      }
-    }, // specifies the color scheme for the component
-    alwaysScrollToBottom: false, // when set to true always scrolls the chat to the bottom when new events are in (new message, user starts typing...)
-    messageStyling: true // enables *bold* /emph/ _underline_ and such (more info at github.com/mattmezza/msgdown)
+    deviceMsg: null
   }),
   methods: {
-    sendMessage(text) {
-      if (text.length > 0) {
-        this.newMessagesCount = this.isChatOpen
-          ? this.newMessagesCount
-          : this.newMessagesCount + 1;
-        this.onMessageWasSent({
-          author: "support",
-          type: "text",
-          data: { text }
-        });
-      }
-    },
-    onMessageWasSent(message) {
-      // called when the user sends a message
-      this.messageList = [...this.messageList, message];
-    },
-    openChat() {
-      // called when the user clicks on the fab button to open the chat
-      this.isChatOpen = true;
-      this.newMessagesCount = 0;
-    },
-    closeChat() {
-      // called when the user clicks on the botton to close the chat
-      this.isChatOpen = false;
-    },
     restFiveHours(time) {
       return addHours(time, -5);
     },
