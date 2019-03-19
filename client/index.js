65380718c30ac61942d137d1b40a2780f60aad62
rktel <rakinakuy@hotmail.com>
2019-03-19 16:23:15 -0500

    deviceMessenger 68

--- client/index.js
+++ client/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import Vuebar from 'vuebar';
 import VueMeteorTracker from 'vue-meteor-tracker'
+import Chat from 'vue-beautiful-chat'
 
 Vue.use(Vuetify)
 Vue.use(Vuebar);
 Vue.use(VueMeteorTracker)
+Vue.use(Chat)
 
 import 'vuetify/dist/vuetify.min.css'
 
