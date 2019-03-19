bc9b5487cb5179c2b0eab615fd9834c2a587527f
rktel <rakinakuy@hotmail.com>
2019-03-19 17:02:25 -0500

    deviceMessenger 75

--- imports/ui/views/components/DeviceMessenger.vue
+++ imports/ui/views/components/DeviceMessenger.vue
@@ -18,13 +18,13 @@
             v-bar
             :style="{height: heightDeviceMessenger-100 +'px' }"
           >
-            <ul>
-              <p v-for="(command,index) in deviceCommands" :key="index">
-                <v-chip color="primary" text-color="white">{{ command.command }}</v-chip>
+            <div class="px-0 mx-0">
+              <p v-for="(command,index) in deviceCommands" :key="index" class="px-0 mx-0">
+                <v-chip color="primary" text-color="white" label>{{ command.command }}</v-chip>
                 <br>
-                <v-chip color="secondary" text-color="white">{{ command.response }}</v-chip>
+                <v-chip color="secondary" text-color="white" label>{{ command.response }}</v-chip>
               </p>
-            </ul>
+            </div>
           </div>
 
           <div class="messengerCommand">
