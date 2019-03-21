300fba11818eb0c685459ec15cc847545e33e8ee
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-21 14:09:15 -0500

    masivo messenger 3

--- imports/ui/views/components/DeviceMassiveMessenger.vue
+++ imports/ui/views/components/DeviceMassiveMessenger.vue
@@ -8,14 +8,14 @@ export default {
       deviceCommands: []
     },
     deviceCommands() {
-      if (this.devicesArraySelected && this.devicesArraySelected.length>0) {
-        let arrayIDs = []
+      if (this.devicesArraySelected && this.devicesArraySelected.length > 0) {
+        let arrayIDs = [];
 
         this.devicesArraySelected.forEach(element => {
-          arrayIDs.push(element.deviceID)
+          arrayIDs.push(element.deviceID);
         });
-        
-        return Commands.find({ deviceID: {$in: arrayIDs}});
+
+        return Commands.find({ deviceID: { $in: arrayIDs } });
       }
     }
   },
@@ -28,12 +28,47 @@ export default {
     },
     devicesArraySelected() {
       return this.$store.getters.devicesArraySelected;
+    },
+    heightDeviceMessenger() {
+      return this.$store.getters.heightDeviceMessenger;
     }
   },
-  data: () => ({}),
+  data: () => ({
+    deviceMsg: null
+  }),
   methods: {
     toggleFlagDMMD(device) {
       this.$store.commit("toggleFlagDMMD");
+    },
+    toUppercase() {
+      if (this.deviceMsg) this.deviceMsg = this.deviceMsg.trim().toUpperCase();
+    },
+    sendMsg() {
+      const devices = this.devicesArraySelected;
+      const message = this.deviceMsg;
+      const persona = this.persona;
+      devices.forEach(device => {
+        switch (device.protocol) {
+          case "Syrus":
+            stSyrus.emit(
+              "SEND_COMMAND_SYRUS",
+              device.deviceID,
+              message,
+              persona
+            );
+            break;
+
+          default:
+            break;
+        }
+      });
+      this.scrollToBottomDM();
+    },
+    scrollToBottomDM() {
+      setTimeout(ns => {
+        const se = document.getElementById("scrollElements");
+        se.scrollTop = se.scrollHeight;
+      }, 450);
     }
   }
 };
@@ -44,7 +79,8 @@ export default {
     <v-dialog v-model="flagDMMD" fullscreen hide-overlay transition="dialog-bottom-transition">
       <v-card>
         <v-toolbar dark color="primary">
-          <v-toolbar-title>Masivo
+          <v-toolbar-title>
+            Masivo
             <v-icon>message</v-icon>
           </v-toolbar-title>
           <v-spacer></v-spacer>
@@ -68,52 +104,54 @@ export default {
             <v-icon right>developer_board</v-icon>
           </v-chip>
 
-        <section class="messengerContent" :style="{height: heightDeviceMessenger +'px' }">
+          <section class="messengerContent" :style="{height: heightDeviceMessenger +'px' }">
+            <div
+              v-bar
+              class="vuebar-element messengerMsg"
+              :style="{height: heightDeviceMessenger-100 +'px' }"
+            >
+              <div class="px-0 mx-0" id="scrollElements">
+                <div v-for="(command,index) in deviceCommands" :key="index" class="px-0 mx-0">
+                  <p
+                    class="text-xs-right my-0 caption grey--text"
+                  >{{command.author}} - {{restFiveHours(command.sendTime)}}</p>
 
-          <div v-bar class="vuebar-element messengerMsg" :style="{height: heightDeviceMessenger-100 +'px' }">
-            <div class="px-0 mx-0" id="scrollElements" >
-              <div v-for="(command,index) in deviceCommands" :key="index" class="px-0 mx-0">
-                <p
-                  class="text-xs-right my-0 caption grey--text"
-                >{{command.author}} - {{restFiveHours(command.sendTime)}}</p>
+                  <p class="text-xs-right my-0">
+                    <v-chip color="secondary" text-color="white" label class="caption">
+                      {{ command.command }}
+                      <v-icon right>account_circle</v-icon>
+                    </v-chip>
+                  </p>
 
-                <p class="text-xs-right my-0">
-                  <v-chip color="secondary" text-color="white" label class="caption">
-                    {{ command.command }}
-                    <v-icon right>account_circle</v-icon>
-                  </v-chip>
-                </p>
-
-                <p
-                  class="my-0 caption ml-2 grey--text text--darken-3"
-                >{{command.deviceID}} - {{restFiveHours(command.receivedTime)}}</p>
-                <p class="my-0">
-                  <v-chip color="white" text-color="black" label class="caption">
-                    <v-avatar>
-                      <v-icon>developer_board</v-icon>
-                    </v-avatar>
-                    {{ command.response }}
-                  </v-chip>
-                </p>
+                  <p
+                    class="my-0 caption ml-2 grey--text text--darken-3"
+                  >{{command.deviceID}} - {{restFiveHours(command.receivedTime)}}</p>
+                  <p class="my-0">
+                    <v-chip color="white" text-color="black" label class="caption">
+                      <v-avatar>
+                        <v-icon>developer_board</v-icon>
+                      </v-avatar>
+                      {{ command.response }}
+                    </v-chip>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="messengerCommand">
-            <v-text-field
-              class="mx-2"
-              v-model="deviceMsg"
-              :append-outer-icon="'send'"
-              clearable
-              label="Message"
-              type="text"
-              @keyup="toUppercase"
-              @keyup.enter.native="sendMsg"
-              @click:append-outer="sendMsg"
-            ></v-text-field>
-          </div>
-        </section>
-
+            <div class="messengerCommand">
+              <v-text-field
+                class="mx-2"
+                v-model="deviceMsg"
+                :append-outer-icon="'send'"
+                clearable
+                label="Message"
+                type="text"
+                @keyup="toUppercase"
+                @keyup.enter.native="sendMsg"
+                @click:append-outer="sendMsg"
+              ></v-text-field>
+            </div>
+          </section>
         </section>
       </v-card>
     </v-dialog>
