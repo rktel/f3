a8042d57d76a232e32d665657ceb8d1aa7d9beb7
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-21 13:56:37 -0500

    masivo messenger 2

--- imports/ui/views/components/DeviceMassiveMessenger.vue
+++ imports/ui/views/components/DeviceMassiveMessenger.vue
@@ -8,11 +8,15 @@ export default {
       deviceCommands: []
     },
     deviceCommands() {
-      /*
-      if (this.deviceMessenger && this.deviceMessenger.deviceID) {
-        return Commands.find({ deviceID: this.deviceMessenger.deviceID });
+      if (this.devicesArraySelected && this.devicesArraySelected.length>0) {
+        let arrayIDs = []
+
+        this.devicesArraySelected.forEach(element => {
+          arrayIDs.push(element.deviceID)
+        });
+        
+        return Commands.find({ deviceID: {$in: arrayIDs}});
       }
-      */
     }
   },
   computed: {
@@ -63,7 +67,53 @@ export default {
             {{ device.deviceID }}
             <v-icon right>developer_board</v-icon>
           </v-chip>
-          
+
+        <section class="messengerContent" :style="{height: heightDeviceMessenger +'px' }">
+
+          <div v-bar class="vuebar-element messengerMsg" :style="{height: heightDeviceMessenger-100 +'px' }">
+            <div class="px-0 mx-0" id="scrollElements" >
+              <div v-for="(command,index) in deviceCommands" :key="index" class="px-0 mx-0">
+                <p
+                  class="text-xs-right my-0 caption grey--text"
+                >{{command.author}} - {{restFiveHours(command.sendTime)}}</p>
+
+                <p class="text-xs-right my-0">
+                  <v-chip color="secondary" text-color="white" label class="caption">
+                    {{ command.command }}
+                    <v-icon right>account_circle</v-icon>
+                  </v-chip>
+                </p>
+
+                <p
+                  class="my-0 caption ml-2 grey--text text--darken-3"
+                >{{command.deviceID}} - {{restFiveHours(command.receivedTime)}}</p>
+                <p class="my-0">
+                  <v-chip color="white" text-color="black" label class="caption">
+                    <v-avatar>
+                      <v-icon>developer_board</v-icon>
+                    </v-avatar>
+                    {{ command.response }}
+                  </v-chip>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="messengerCommand">
+            <v-text-field
+              class="mx-2"
+              v-model="deviceMsg"
+              :append-outer-icon="'send'"
+              clearable
+              label="Message"
+              type="text"
+              @keyup="toUppercase"
+              @keyup.enter.native="sendMsg"
+              @click:append-outer="sendMsg"
+            ></v-text-field>
+          </div>
+        </section>
+
         </section>
       </v-card>
     </v-dialog>
