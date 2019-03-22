593ee365f6803587e6f122cbe53aa01245dd5544
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-22 13:28:13 -0500

    deviceScript 9

--- imports/ui/views/components/DeviceScript.vue
+++ imports/ui/views/components/DeviceScript.vue
@@ -3,9 +3,7 @@ import { stSyrus } from "../../../api/streamers.js";
 import { Commands } from "../../../api/collections.js";
 import { addHours } from "../../../tools/time.js";
 export default {
-  meteor: {
-
-  },
+  meteor: {},
   computed: {
     persona() {
       return this.$store.getters.persona;
@@ -21,7 +19,7 @@ export default {
     }
   },
   data: () => ({
-
+    value: 0
   }),
   methods: {
     scrollToBottomDM() {
@@ -36,8 +34,7 @@ export default {
     toggleFlagDSD(device) {
       this.$store.commit("toggleFlagDSD");
       this.$store.commit("setDeviceScript", {});
-    },
-
+    }
   }
 };
 </script>
@@ -47,7 +44,10 @@ export default {
     <v-dialog v-model="flagDSD" fullscreen hide-overlay transition="dialog-bottom-transition">
       <v-card>
         <v-toolbar dark color="primary">
-          <v-toolbar-title><v-icon>insert_drive_file</v-icon>{{deviceScript.deviceID}}</v-toolbar-title>
+          <v-toolbar-title>
+            <v-icon>insert_drive_file</v-icon>
+            {{deviceScript.deviceID}}
+          </v-toolbar-title>
           <v-spacer></v-spacer>
           <v-toolbar-items>
             <v-btn icon dark @click="toggleFlagDSD">
@@ -55,56 +55,44 @@ export default {
             </v-btn>
           </v-toolbar-items>
         </v-toolbar>
-<section>
-  <v-card
-    class="mx-auto"
-    color="#26c6da"
-    dark
-    max-width="400"
-  >
-    <v-card-title>
-      <v-icon
-        large
-        left
-      >
-        mdi-twitter
-      </v-icon>
-      <span class="title font-weight-light">Twitter</span>
-    </v-card-title>
+        <section>
+          <v-card class="mx-auto" color="#26c6da" dark max-width="400">
+            <v-card-title>
+              <v-icon large left>mdi-twitter</v-icon>
+              <span class="title font-weight-light">Twitter</span>
+            </v-card-title>
 
-    <v-card-text class="headline font-weight-bold">
-      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
-    </v-card-text>
+            <v-card-text
+              class="headline font-weight-bold"
+            >"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."</v-card-text>
 
-    <v-card-actions>
-      <v-list-tile class="grow">
-        <v-list-tile-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-        </v-list-tile-avatar>
+            <v-card-actions>
+              <v-list-tile class="grow">
+                <v-list-tile-avatar color="grey darken-3">
+                  <v-progress-circular
+                    :rotate="-90"
+                    :size="100"
+                    :width="15"
+                    :value="value"
+                    color="primary"
+                  >{{ value }}</v-progress-circular>
+                </v-list-tile-avatar>
 
-        <v-list-tile-content>
-          <v-list-tile-title>Evan You</v-list-tile-title>
-        </v-list-tile-content>
+                <v-list-tile-content>
+                  <v-list-tile-title>Evan You</v-list-tile-title>
+                </v-list-tile-content>
 
-        <v-layout
-          align-center
-          justify-end
-        >
-          <v-icon class="mr-1">mdi-heart</v-icon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-          <v-icon class="mr-1">mdi-share-variant</v-icon>
-          <span class="subheading">45</span>
-        </v-layout>
-      </v-list-tile>
-    </v-card-actions>
-  </v-card>
-
-</section>
- 
+                <v-layout align-center justify-end>
+                  <v-icon class="mr-1">mdi-heart</v-icon>
+                  <span class="subheading mr-2">256</span>
+                  <span class="mr-1">·</span>
+                  <v-icon class="mr-1">mdi-share-variant</v-icon>
+                  <span class="subheading">45</span>
+                </v-layout>
+              </v-list-tile>
+            </v-card-actions>
+          </v-card>
+        </section>
       </v-card>
     </v-dialog>
   </v-layout>
