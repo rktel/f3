c049011b90c2d8e6f6d094e7906d0e32351fa735
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-26 17:59:41 -0500

    taskScript 14

--- imports/ui/views/components/DeviceScript.vue
+++ imports/ui/views/components/DeviceScript.vue
@@ -103,14 +103,13 @@ export default {
             <v-card-actions>
               <v-list-tile class="grow">
                 <v-list-tile-avatar color="grey darken-3">
-                  <p>{{taskName}}</p>
                   <v-progress-circular
                     :rotate="-90"
                     :size="100"
-                    :width="30"
+                    :width="15"
                     :value="value"
                     color="primary"
-                  >{{ value }}%</v-progress-circular>
+                  >{{ value + '%' }}</v-progress-circular>
                 </v-list-tile-avatar>
 
                 <v-list-tile-content>
