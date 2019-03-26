816051877cccdbb0c4ba7c6ae61edc85749f59c7
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-03-26 18:11:34 -0500

    taskScript 19

--- imports/ui/views/components/DeviceScript.vue
+++ imports/ui/views/components/DeviceScript.vue
@@ -103,7 +103,7 @@ export default {
             <v-card-actions>
               <v-list-tile class="grow">
                 <v-list-tile-avatar color="grey darken-3">
-                  <v-progress-circular :style="{ margin: 10 + 'rem'}"
+                  <v-progress-circular
                     :rotate="-90"
                     :size="100"
                     :width="15"
