63733b2403431b9242cc9c6567a0aeca55db721d
rktel <rakinakuy@hotmail.com>
2019-07-05 16:14:10 -0500

    avl-main

--- server/main.js
+++ server/main.js
@@ -2,8 +2,8 @@ import { Syrus } from '../server/gps/dct/syrus_adapter'
 
 
 Meteor.startup(() => {
-    Meteor.call('devicesReset')
-    Meteor.call('createHyperPersonal')
-    Meteor.call('setEmailOptions')
-    new Syrus()
+   // Meteor.call('devicesReset')
+   // Meteor.call('createHyperPersonal')
+   // Meteor.call('setEmailOptions')
+   // new Syrus()
 })
