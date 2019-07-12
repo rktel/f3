c269202e8a815d8b6fbb80652f08d571875877c0
rktel <rakinakuy@hotmail.com>
2019-07-12 11:09:37 -0500

    disable SYrus 4

--- server/avl/avl-main.js
+++ server/avl/avl-main.js
@@ -14,7 +14,8 @@ server.on('connection', function(sock) {
     sockets.push(sock);
 
     sock.on('data', function(data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        //console.log('DATA ' + sock.remoteAddress + ': ' + data);
+         console.log(data);
         // Write the data back to all the connected, the client will receive it as data from the server
         sockets.forEach(function(sock, index, array) {
            // sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
