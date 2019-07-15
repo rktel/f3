498bc5f11119f28595e8551d00c6f5aa186d703c
rktel <rakinakuy@hotmail.com>
2019-07-14 22:04:02 -0500

    SOCK 13

--- server/avl/avl-server.js
+++ server/avl/avl-server.js
@@ -24,14 +24,14 @@ export function Server(port, host) {
                 const SOCK_MASTER = getSOCK(sockIndex);
 
                 // mobileID exist on SOCK_MASTER! 
-                if (SOCK_MASTER && SOCK_MASTER.find(element => {if(element){ element.mobileID === mobileID}else{return false}})) {
+                if (SOCK_MASTER && SOCK_MASTER.find(element => {if(element){ return element.mobileID === mobileID}})) {
                     const elementIndex = SOCK_MASTER.findIndex(element => element.mobileID == mobileID);
                     sock.mobileID = mobileID;
                     SOCK_MASTER[elementIndex] = sock;
                     console.log('exit SOCK_MASTER:', SOCK_MASTER.length);
                 }
                 // mobileID No exist on SOCK_MASTER! 
-                if (SOCK_MASTER && !SOCK_MASTER.find(element => {if(element){ element.mobileID === mobileID}else{return false}})) {
+                if (SOCK_MASTER && !SOCK_MASTER.find(element => {if(element){ return element.mobileID === mobileID}})) {
                     sock.mobileID = mobileID;
                     SOCK_MASTER.push(sock);
                     console.log('NO exist SOCK_MASTER:', SOCK_MASTER.length);
