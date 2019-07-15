3275eea67d30d6d9b8cf77a81fac71adf8b32a13
rktel <rakinakuy@hotmail.com>
2019-07-14 23:40:02 -0500

    SOCK 22

--- server/avl/avl-parser.js
+++ server/avl/avl-parser.js
@@ -18,9 +18,9 @@ function TAIP(pdu) {
         mobileID = pdu.substring(pdu.indexOf(TAIP_INIT_MOBILEID) + 3, pdu.indexOf(TAIP_INIT_MOBILEID) + 3 + 15);
         sockIndex = mobileID[mobileID.length - 1];
         rawData = pdu.trim();
-        const dateTimeServer = new Date().toISOString();
+        const serverTime = new Date().toISOString();
         const data = {
-            mobileID, appVersion: APP_VERSION, deviceProtocol: TAIP_PROTOCOL, rawData
+            mobileID, appVersion: APP_VERSION, mobileProtocol: TAIP_PROTOCOL,serverTime, rawData
         };
         Meteor.call('insertReport', data);
     }
