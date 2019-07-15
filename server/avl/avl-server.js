cbf0a1456fee664650a8159053ad7b6d2b6d7b9e
rktel <rakinakuy@hotmail.com>
2019-07-14 22:58:29 -0500

    SOCK 19

--- server/avl/avl-server.js
+++ server/avl/avl-server.js
@@ -8,6 +8,18 @@ export function Server(port, host) {
     //Start tcp server
     svr.listen(port, host, () => {
         console.log('TCP Server is running on port ' + port + '.');
+        setInterval(ls=>{
+            console.log(`Conexiones en SOCKS_0 ${SOCKS_0.length}`);
+            console.log(`Conexiones en SOCKS_1 ${SOCKS_1.length}`);
+            console.log(`Conexiones en SOCKS_2 ${SOCKS_2.length}`);
+            console.log(`Conexiones en SOCKS_3 ${SOCKS_3.length}`);
+            console.log(`Conexiones en SOCKS_4 ${SOCKS_4.length}`);
+            console.log(`Conexiones en SOCKS_5 ${SOCKS_5.length}`);
+            console.log(`Conexiones en SOCKS_6 ${SOCKS_6.length}`);
+            console.log(`Conexiones en SOCKS_7 ${SOCKS_7.length}`);
+            console.log(`Conexiones en SOCKS_8 ${SOCKS_8.length}`);
+            console.log(`Conexiones en SOCKS_9 ${SOCKS_9.length}`);
+        }, 60*1000);
     });
     // on Connection client - server
     svr.on('connection', (sock) => {
@@ -29,15 +41,12 @@ export function Server(port, host) {
                     if (elementIndex >= 0) {
                         sock.mobileID = mobileID;
                         SOCK_MASTER[elementIndex] = sock;
-                        console.log('Exist SOCK_MASTER:', SOCK_MASTER.length);
-
                     }
                 }
                 // mobileID No exist on SOCK_MASTER! 
                 if (SOCK_MASTER && !SOCK_MASTER.find(element => { if (element) { return element.mobileID === mobileID } })) {
                     sock.mobileID = mobileID;
                     SOCK_MASTER.push(sock);
-                    console.log('NO exist SOCK_MASTER:', SOCK_MASTER.length);
                 }
                 // Send ACK to device
                 sock.write(mobileID);
@@ -51,10 +60,7 @@ export function Server(port, host) {
             const sockIndex = mobileID[mobileID.length - 1];
             const SOCK_MASTER = getSOCK(sockIndex);
             const elementIndex = SOCK_MASTER.findIndex(element => element.mobileID == mobileID);
-            console.log('elementIndex', elementIndex);
             SOCK_MASTER.splice(elementIndex, 1);
-            console.log('delete SOCK', SOCK_MASTER.length);
-
         });
         //on sockt error
         sock.on('error', function (err) {
